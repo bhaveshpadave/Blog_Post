3bc404e93654a9d382998269a65df769105f85af
bhaveshpadave <bhaveshpadave97@gmail.com>
2024-04-10 12:55:11 +0530

    Issue fix: remove, edit, update

--- index.js
+++ index.js
@@ -4,155 +4,110 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-var blogArray = 0;
-var blogCount = 6;
-
-//Object skeleton for new and edit blog
-var newUrl, newTitle, newDescription;
-const blogData = {
-    url: newUrl,
-    title: newTitle,
-    description: newDescription
-}
-
-const blog1 = {
+const allBlogs = [{
     url: "/blog1",
-    title: "The apoclypse",
-    description: "Some quick example text to build on the card title and make up the bulk of the card's content. It was apoclypse experience",
+    title: "How to Get New Ideas",
+    description: "The way to get new ideas is to notice anomalies: what seems strange, or missing, or broken? You can see anomalies in everyday life (much of standup comedy is based on this), but the best place to look for them is at the frontiers of knowledge. Knowledge grows fractally. From a distance its edges look smooth, but when you learn enough to get close to one, you'll notice it's full of gaps. These gaps will seem obvious; it will seem inexplicable that no one has tried x or wondered about y. In the best case, exploring such gaps yields whole new fractal buds.",
     id: "blog1"
-};
-const blog2 = {
+},
+{
     url: "/blog2",
-    title: "New Era",
-    description: "Some quick example text to build on the card title and make up the bulk of the card's content. The begining of advance future",
+    title: "An NFT That Saves Lives",
+    description: "Noora Health, a nonprofit I've supported for years, just launched a new NFT. It has a dramatic name, Save Thousands of Lives, because that's what the proceeds will do. Noora has been saving lives for 7 years. They run programs in hospitals in South Asia to teach new mothers how to take care of their babies once they get home. They're in 165 hospitals now. And because they know the numbers before and after they start at a new hospital, they can measure the impact they have. It is massive. For every 1000 live births, they save 9 babies.",
     id: "blog2"
-};
-const blog3 = {
+},
+{
     url: "/blog3",
-    title: "Civil Wars",
-    description: "Some quick example text to build on the card title and make up the bulk of the card's content. The world is on the edge of war",
+    title: "Two Types",
+    description: "I think you only need two kinds of people to create a technology hub: rich people and nerds. They're the limiting reagents in the reaction that produces startups, because they're the only ones present when startups get started. Everyone else will move. Observation bears this out: within the US, towns have become startup hubs if and only if they have both rich people and nerds. Few startups happen in Miami, for example, because although it's full of rich people, it has few nerds. It's not the kind of place nerds like.",
     id: "blog3"
-};
-const blog4 = {
-    url: "/blog4",
-    title: "The apoclypse",
-    description: "It was apoclypse experience",
-    id: "blog4"
-};
-const blog5 = {
-    url: "/blog5",
-    title: "New Era",
-    description: "The begining of advance future",
-    id: "blog5"
-};
-const blog6 = {
-    url: "/blog6",
-    title: "Civil Wars",
-    description: "The world is on the edge of war",
-    id: "blog6"
-};
-
-var allBlogs = [blog1, blog2, blog3, blog4, blog5, blog6];
+},
+// {
+//     url: "/blog4",
+//     title: "The apoclypse",
+//     description: "It was apoclypse experience",
+//     id: "blog4"
+// },
+// {
+//     url: "/blog5",
+//     title: "New Era",
+//     description: "The begining of advance future",
+//     id: "blog5"
+// },
+// {
+//     url: "/blog6",
+//     title: "Civil Wars",
+//     description: "The world is on the edge of war",
+//     id: "blog6"
+// }
+];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Start of index.ejs 
+// Show all blog posts
 app.get("/", (req,res) => {
     res.render("index.ejs", {
         blog: allBlogs,
-        showBLogsFun: showAllBlogs()
     });
     
 });
 
-// Start of blog.ejs
-function blog (array){
-    blogArray = array;
-    var array = Object.create(array);
-    app.get(array.url, (req, res) => {
-        res.render("blog.ejs", {
-            url: array.url,
-            title: array.title,
-            description: array.description,
-            id: array.id
-        });
+// Read a blog post
+app.get("/blog/:id", (req, res) => {
+    const i =  allBlogs.findIndex((e)=> e.id === req.params.id);
+    const array = allBlogs[i];
+    res.render("blog.ejs", {
+        blog: array,
     });
-}
-// End of blog.ejs
+});
 
-function showAllBlogs(){
-    allBlogs.forEach(e => {
-        blog(e);
-    });
-}
-// End of index.ejs
-
-// Start of new_blog.ejs
+// Create new blog post
 app.get("/create-new-blog", (req, res) => {
     res.render("new_blog.ejs");
 });
-
 app.post("/create-new-blog", (req, res) => {
-    blogCount++;
-    res.render("new_blog.ejs", {
-        // url: req.body.blogUrl,
+    let arrayName = "blog" + (allBlogs.length+1);
+    var urlgen = "/" + arrayName;
+
+    let blogArray = {
+        url: `${urlgen}`,
         title: req.body.blogTitle,
-        description: req.body.blogDescription
-    });
+        description: req.body.blogDescription,
+        id: `${arrayName}`
+    }
 
-    var arrayName = "blog" + blogCount;
-    var urlgen = "/"+arrayName;
+    allBlogs.push(blogArray);
 
-    arrayName = Object.create(blogData);
-
-    arrayName.url = urlgen;
-    arrayName.title = req.body.blogTitle;
-    arrayName.description = req.body.blogDescription;
-    arrayName.id = `${arrayName}`;
-
-    allBlogs.push(arrayName);
-    console.log(allBlogs[6].valueOf());
-
+    res.redirect("/");
 });
-// End of new_blog.ejs
 
-// Start of edit_blog.ejs
-app.get("/edit-blog", (req, res) => {
+// Edit/ update existing blog post
+app.get("/blog/edit/:id", (req, res) => {
+    const i =  allBlogs.findIndex((e)=> e.id === req.params.id);
+    const array = allBlogs[i];
         res.render("edit_blog.ejs", {
-            blogUrl: blogArray.url,
-            blogTitle: blogArray.title,
-            blogDescription: blogArray.description
+            blogId: req.params.id,
+            blogUrl: array.url,
+            blogTitle: array.title,
+            blogDescription: array.description
     });
-
+});
+app.post("/blog/edit/:id", (req, res) => {
+    const i =  allBlogs.findIndex((e)=> e.id === req.params.id);
+    const array = allBlogs[i];
+    array.title = req.body.newBlogTitle;
+    array.description = req.body.newBlogDescription;
+    res.redirect("/");
 });
 
-app.post("/", (req, res) => {
-
-    res.render("edit_blog.ejs", {
-        title: req.body['newBlogTitle'],
-        description: req.body['newBlogDescription'],
-        blogTitle: req.body['newBlogTitle'],
-        blogDescription: req.body['newBlogDescription']
-    });
-
-    blogArray.title = req.body.newBlogTitle;
-    blogArray.description = req.body.newBlogDescription;
-
-});
-// End of edit_blog.ejs
-
-// Start of remove blog
-app.get("/remove-blog", (req, res) => {
-    var i = allBlogs.indexOf(`${req.body.id}`);
-
-    console.log(i);
-    console.log(blogArray);
-
+// Remove a blog post
+app.get("/remove/:id", (req, res) => {
+    const i =  allBlogs.findIndex((e)=> e.id === req.params.id);
     allBlogs.splice(i,1);
     res.render("remove_blog.ejs");
 });
-// End of remove blog
+
 
 //Console log to check the port
 app.listen(PORT, (req, res) => {
